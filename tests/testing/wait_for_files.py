edf7fe9bf04643452807db1272476bfe34c963a8
Buck Golemon <buck@yelp.com>
2015-10-23 13:18:25 -0700

    coverage debugging: add tiestamps, wait longer on successive waits

--- tests/testing/wait_for_files.py
+++ tests/testing/wait_for_files.py
@@ -9,17 +9,31 @@ from __future__ import unicode_literals
 from glob import glob
 from os.path import getmtime
 from time import sleep
-from time import time
+from time import time as now
+
+inf = float('inf')
+
+
+def format_time(timestamp):
+    from datetime import datetime
+    return datetime.fromtimestamp(timestamp).strftime('%F %T.%f')
 
 
 def wait_for_files(pattern, min_age):
     while True:
-        for path in glob(pattern):
-            age = time() - getmtime(path)
-            if age <= min_age:
-                print('%s: only %.3gs old' % (path, age))
-                sleep(min_age - age)
-                break
+        max_mtime, max_path = max((getmtime(path), path) for path in glob(pattern))
+        age = now() - max_mtime
+        if age <= min_age:
+            delta = min_age - age
+            print('%s mtime:%s delta:%g %s: only %g old' % (
+                format_time(now()),
+                format_time(max_mtime),
+                delta,
+                max_path,
+                age,
+            ))
+            min_age += delta
+            sleep(min_age - age)
         else:
             break
 
